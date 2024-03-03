c4b99d2e8fbed996fcb0ec3c9b158bde9429b525
IsukyPohlo <gnivri_los@hotmail.com>
2024-03-02 22:45:35 -0600

    few fixes

--- Scenes/Char.gd
+++ Scenes/Char.gd
@@ -58,6 +58,8 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta):
 	
+	# Very based on: https://github.com/LegionGames/FirstPersonController
+	
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
@@ -65,7 +67,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	# Object handling
+	# Object handling based on: https://www.youtube.com/watch?v=jLIe1_xvOXU
 	if pickedObject != null:
 		var objPos = pickedObject.global_transform.origin
 		var handPos = hand.global_transform.origin
